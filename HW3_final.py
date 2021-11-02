f1bc01a0a88460b8bdf824e73dc043c325958c67
MikhailMP1 <81079449+MikhailMP1@users.noreply.github.com>
2021-11-02 15:06:55 +0300

    Add files via upload

--- HW3_final.py
+++ HW3_final.py
@@ -1,43 +1,40 @@
 class CountVectorizer(object):
-    @staticmethod
-    def fit_transform(raw_document):
+    def fit_transform(self, raw_document):
+        self.raw_document = raw_document
         col = 0
         text = {}
-        n_rows = 0
         for row in raw_document:
-            n_rows += 1
-            for word in row.split():
-                if word.lower() not in text.keys():
-                    text[word.lower()] = col
+            for word in row.lower().split():
+                if word not in text.keys():
+                    text[word] = col
                     col += 1
         text_words = list(text.keys())
-        vector = [0 for i in range((max(text.values())) + 1)]
-        matrix = [vector.copy() for i in range(n_rows)]
+        vector = [0 for i in range(col)]
+        matrix = [vector.copy() for i in range(len(raw_document))]
 
         num_row = 0
         for row in raw_document:
-            for word in row.split():
-                matrix[num_row][text_words.index(word.lower())] += 1
+            for word in row.lower().split():
+                matrix[num_row][text_words.index(word)] += 1
             num_row += 1
         return matrix
 
-    @staticmethod
-    def get_feature_name(raw_document):
+    def get_feature_name(self):
         words_li = []
-        for row in raw_document:
-            for word in row.split():
-                if word.lower() not in words_li:
-                    words_li.append(word.lower())
+        for row in self.raw_document:
+            for word in row.lower().split():
+                if word not in words_li:
+                    words_li.append(word)
         return words_li
 
+if __name__ == '__main__':
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste',
+        'say no to racism'
+    ]
 
-corpus = [
-    'Crock Pot Pasta Never boil pasta again',
-    'Pasta Pomodoro Fresh ingredients Parmesan to taste',
-    'say no to racism'
-]
-
-vectorizer = CountVectorizer()
-count_matrix = vectorizer.fit_transform(corpus)
-print(vectorizer.get_feature_name(corpus))
-print(count_matrix)
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_name())
+    print(count_matrix)
