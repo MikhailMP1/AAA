ebca98fa50665bcc2b62c052d13c538963235989
MikhailMP1 <81079449+MikhailMP1@users.noreply.github.com>
2021-11-13 02:40:12 +0300

    Add files via upload

--- HW4.py
+++ HW4.py
@@ -2,14 +2,17 @@ import json
 
 
 class ColorizeMixin:
+    """ Определяет цвет вывода метода repr в классе Advert."""
 
     repr_color_code = 32
 
     def colorize(self, repr_color_code):
-        return f"\033[1;{repr_color_code};40m {self.__repr__()}  \n"
+        return f"\033[1;{repr_color_code};40m {self.__repr__()}  \033[1;37;40m "
 
 
 class Advert(ColorizeMixin, dict):
+    """Динамичесĸи создает атрибуты эĸземпляра ĸласса из атрибутов JSON-объеĸта.
+       Позволяет обращаться к ним через точку."""
 
     def __getattr__(self, value):
         if not isinstance(self[value], dict):
@@ -19,6 +22,10 @@ class Advert(ColorizeMixin, dict):
 
     @property
     def price(self):
+
+        """проверяет, что устанавливаемое значение цены не отрицательное.
+            В случае отсутствия возвращает 0."""
+
         if self.get('price', 0) < 0:
             return 'ValueError: must be >= 0'
         else:
@@ -30,30 +37,17 @@ class Advert(ColorizeMixin, dict):
 
 if __name__ == '__main__':
     lesson_str = """{
-                    "title": "python",
-                    "price": 0,
+                    "title": "iPhone X",
+                    "price": 100,
                     "location": {
-                    "address": "город Москва, Лесная, 7",
-                    "metro_stations": ["Белорусская"]
+                    "address": "город Самара, улица Мориса Тореза, 50",
+                    "metro_stations": ["Спортивная", "Гагаринская"]
                     }
                     }"""
 
     lesson = json.loads(lesson_str)
     lesson_ad = Advert(lesson)
 
+    print(lesson_ad.colorize(repr_color_code=33))
     print(lesson_ad.price)
     print(lesson_ad.location.address)
-    print(lesson_ad.colorize(repr_color_code=32))
-
-    iphone = {
-        "title": "iPhone X",
-        "price": -1,
-        "location": {
-            "address": "город Самара, улица Мориса Тореза, 50",
-            "metro_stations": ["Спортивная", "Гагаринская"]
-        }
-    }
-    iphone_ad = Advert(iphone)
-    print(iphone_ad.price)
-    print(iphone_ad.location.address)
-    print(iphone_ad.colorize(repr_color_code=37))
